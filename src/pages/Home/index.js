ce3c087a375e0c77c87f56b2ebbdc0e47ff9ecf9
MisterBakka <130415011+MisterBakka@users.noreply.github.com>
2023-09-19 18:52:07 +0200

    commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,10 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData()
+  const last = data && data.events && data.events.length > 0
+      ? data.events[data.events.length -1]
+      : null;
   return <>
     <header>
       <Menu />
@@ -115,7 +118,8 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
+        <h3>Notre dernière prestation</h3>
+        {last && (
         <EventCard
           imageSrc={last?.cover}
           title={last?.title}
@@ -123,6 +127,7 @@ const Page = () => {
           small
           label="boom"
         />
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
